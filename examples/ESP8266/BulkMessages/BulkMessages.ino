c8c096f73463604d8cee1feaf91314c1c2acf01b
J-Rios <jose_23991@hotmail.com>
2018-07-18 21:36:56 +0200

    Removed String based examples and new implementation untested ones

--- examples/ESP8266/BulkMessages/BulkMessages.ino
+++ examples/ESP8266/BulkMessages/BulkMessages.ino
@@ -1,8 +1,9 @@
 /*******************************************************************
-*  An example of how to use a bulk messages to subscribed users.  *
-*                                                                 *
-*                                                                 *
-*  written by Vadim Sinitski                                      *
+*  An example of how to use a bulk messages to subscribed users.   *
+*                                                                  *
+*                                                                  *
+*  written by Vadim Sinitski                                       *
+*  modified by Jose Rios                                           *
 *******************************************************************/
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
@@ -20,88 +21,24 @@ char password[] = "YYYYYY"; // your network key
 WiFiClientSecure secured_client;
 UniversalTelegramBot bot(BOTtoken, secured_client);
 
-int Bot_mtbs = 1000; // mean time between scan messages
-long Bot_lasttime;   // last time messages' scan has been done
+unsigned long Bot_mtbs = 1000; // mean time between scan messages
+unsigned long Bot_lasttime;   // last time messages' scan has been done
 
 int bulk_messages_mtbs = 1500; // mean time between send messages, 1.5 seconds
 int messages_limit_per_second = 25; // Telegram API have limit for bulk messages ~30 messages per second
 
-String subscribed_users_filename = "subscribed_users.json";
+const char subscribed_users_filename[] = "/subscribed_users.json";
 
 DynamicJsonBuffer jsonBuffer;
 
-void handleNewMessages(int numNewMessages) {
-  Serial.println("handleNewMessages");
-  Serial.println(String(numNewMessages));
-
-  for (int i=0; i<numNewMessages; i++) {
-    String chat_id = String(bot.messages[i].chat_id);
-    String text = bot.messages[i].text;
-
-    String from_name = bot.messages[i].from_name;
-    if (from_name == "") from_name = "Guest";
-
-    if (text == "/start") {
-      if (addSubscribedUser(chat_id, from_name)) {
-        String welcome = "Welcome to Universal Arduino Telegram Bot library, " + from_name + ".\n";
-        welcome += "This is Bulk Messages example.\n\n";
-        welcome += "/showallusers : show all subscribed users\n";
-        welcome += "/testbulkmessage : send test message to subscribed users\n";
-        welcome += "/removeallusers : remove all subscribed users\n";
-        welcome += "/stop : unsubscribe from bot\n";
-        bot.sendMessage(chat_id, welcome, "Markdown");
-      } else {
-        bot.sendMessage(chat_id, "Something wrong, please try again (later?)", "");
-      }
-    }
-
-    if (text == "/stop") {
-      if (removeSubscribedUser(chat_id)) {
-        bot.sendMessage(chat_id, "Thank you, " + from_name + ", we always waiting you back", "");
-      } else {
-        bot.sendMessage(chat_id, "Something wrong, please try again (later?)", "");
-      }
-    }
-
-    if (text == "/testbulkmessage") {
-      sendMessageToAllSubscribedUsers("ATTENTION, this is bulk message for all subscribed users!");
-    }
-
-    if (text == "/showallusers") {
-      File subscribedUsersFile = SPIFFS.open("/"+subscribed_users_filename, "r");
-
-      if (!subscribedUsersFile) {
-        bot.sendMessage(chat_id, "No subscription file", "");
-      }
-
-      size_t size = subscribedUsersFile.size();
-
-      if (size > 1024) {
-        bot.sendMessage(chat_id, "Subscribed users file is too large", "");
-      } else {
-        String file_content = subscribedUsersFile.readString();
-        bot.sendMessage(chat_id, file_content, "");
-      }
-    }
-
-    if (text == "/removeallusers") {
-      if (SPIFFS.remove("/"+subscribed_users_filename)) {
-        bot.sendMessage(chat_id, "All users removed", "");
-      } else {
-        bot.sendMessage(chat_id, "Something wrong, please try again (later?)", "");
-      }
-    }
-  }
-}
-
 JsonObject& getSubscribedUsers() {
-  File subscribedUsersFile = SPIFFS.open("/"+subscribed_users_filename, "r");
+  File subscribedUsersFile = SPIFFS.open(subscribed_users_filename, "r");
 
   if (!subscribedUsersFile) {
     Serial.println("Failed to open subscribed users file");
 
     // Create empty file (w+ not working as expect)
-    File f = SPIFFS.open("/"+subscribed_users_filename, "w");
+    File f = SPIFFS.open(subscribed_users_filename, "w");
     f.close();
 
     JsonObject& users = jsonBuffer.createObject();
@@ -116,7 +53,10 @@ JsonObject& getSubscribedUsers() {
       //return users;
     }
 
-    String file_content = subscribedUsersFile.readString();
+    static char file_content[1024];
+    memset(file_content, '\0', 1024);
+    subscribedUsersFile.readBytes(file_content, 1024);
+    file_content[1023] = '\0';
 
     JsonObject& users = jsonBuffer.parseObject(file_content);
 
@@ -131,10 +71,10 @@ JsonObject& getSubscribedUsers() {
   }
 }
 
-bool addSubscribedUser(String chat_id, String from_name) {
+bool addSubscribedUser(const char* chat_id, const char* from_name) {
   JsonObject& users = getSubscribedUsers();
 
-  File subscribedUsersFile = SPIFFS.open("/"+subscribed_users_filename, "w+");
+  File subscribedUsersFile = SPIFFS.open(subscribed_users_filename, "w+");
 
   if (!subscribedUsersFile) {
     Serial.println("Failed to open subscribed users file for writing");
@@ -149,10 +89,10 @@ bool addSubscribedUser(String chat_id, String from_name) {
   return true;
 }
 
-bool removeSubscribedUser(String chat_id) {
+bool removeSubscribedUser(const char* chat_id) {
   JsonObject& users = getSubscribedUsers();
 
-  File subscribedUsersFile = SPIFFS.open("/"+subscribed_users_filename, "w");
+  File subscribedUsersFile = SPIFFS.open(subscribed_users_filename, "w");
 
   if (!subscribedUsersFile) {
     Serial.println("Failed to open subscribed users file for writing");
@@ -167,7 +107,7 @@ bool removeSubscribedUser(String chat_id) {
   return true;
 }
 
-void sendMessageToAllSubscribedUsers(String message) {
+void sendMessageToAllSubscribedUsers(const char* message) {
   int users_processed = 0;
 
   JsonObject& users = getSubscribedUsers();
@@ -181,6 +121,84 @@ void sendMessageToAllSubscribedUsers(String message) {
     } else {
       delay(bulk_messages_mtbs);
       users_processed = 0;
+    }
+  }
+}
+
+void handleNewMessages(int numNewMessages) {
+  char* chat_id;
+  char* text;
+  char* from_name;
+
+  Serial.println("handleNewMessages");
+  Serial.println(numNewMessages);
+
+  for (int i=0; i<numNewMessages; i++) {
+    chat_id = bot.messages[i].chat_id;
+    text = bot.messages[i].text;
+
+    from_name = bot.messages[i].from_name;
+    if (strcmp(from_name, "") == 0)
+      from_name = (char*)"Guest";
+
+    if (strcmp(text, "/start") == 0) {
+      if (addSubscribedUser(chat_id, from_name)) {
+        static char welcome[128];
+        memset(welcome, '\0', 128);
+        sprintf(welcome, "Welcome to Universal Arduino Telegram Bot library, %s.\n"
+          "This is Bulk Messages example.\n"
+          "\n"
+          "/showallusers : show all subscribed users\n"
+          "/testbulkmessage : send test message to subscribed users\n"
+          "/removeallusers : remove all subscribed users\n"
+          "/stop : unsubscribe from bot\n", from_name);
+        bot.sendMessage(chat_id, welcome, "Markdown");
+      } else {
+        bot.sendMessage(chat_id, "Something wrong, please try again (later?)", "");
+      }
+    }
+
+    if (strcmp(text, "/stop") == 0) {
+      if (removeSubscribedUser(chat_id)) {
+          char to_print[64];
+          to_print[0] = '\0';
+          sprintf(to_print, "Thank you, %s , we always waiting you back", from_name);
+        bot.sendMessage(chat_id, to_print, "");
+      } else {
+        bot.sendMessage(chat_id, "Something wrong, please try again (later?)", "");
+      }
+    }
+
+    if (strcmp(text, "/testbulkmessage") == 0) {
+      sendMessageToAllSubscribedUsers("ATTENTION, this is bulk message for all subscribed users!");
+    }
+
+    if (strcmp(text, "/showallusers") == 0) {
+      File subscribedUsersFile = SPIFFS.open(subscribed_users_filename, "r");
+
+      if (!subscribedUsersFile) {
+        bot.sendMessage(chat_id, "No subscription file", "");
+      }
+
+      size_t size = subscribedUsersFile.size();
+
+      if (size > 1024) {
+        bot.sendMessage(chat_id, "Subscribed users file is too large", "");
+      } else {
+        static char file_content[1024];
+        memset(file_content, '\0', 1024);
+        subscribedUsersFile.readBytes(file_content, 1024);
+        file_content[1023] = '\0';
+        bot.sendMessage(chat_id, file_content, "");
+      }
+    }
+
+    if (strcmp(text, "/removeallusers") == 0) {
+      if (SPIFFS.remove(subscribed_users_filename)) {
+        bot.sendMessage(chat_id, "All users removed", "");
+      } else {
+        bot.sendMessage(chat_id, "Something wrong, please try again (later?)", "");
+      }
     }
   }
 }
--- examples/ESP8266/EchoBot/EchoBot.ino
+++ examples/ESP8266/EchoBot/EchoBot.ino
@@ -18,8 +18,8 @@ char password[] = "YYYYYY"; // your network key
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
-int Bot_mtbs = 1000; //mean time between scan messages
-long Bot_lasttime;   //last time messages' scan has been done
+unsigned long Bot_mtbs = 1000; //mean time between scan messages
+unsigned long Bot_lasttime;   //last time messages' scan has been done
 
 void setup() {
   Serial.begin(115200);
